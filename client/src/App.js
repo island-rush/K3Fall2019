f247196a842f8e11ed8215ace89805ac4a92fd44
spenceradolph <spenceradolph@ymail.com>
2019-07-15 20:57:17 -0500

    create react app boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is the Island Rush Game...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
