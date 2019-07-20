3aea6ea3b291b91858935d404d65813a787ad8ea
spenceradolph <spenceradolph@ymail.com>
2019-07-20 16:06:07 -0500

    removing temp sql

--- server/sql/k3_tableInsert.sql
+++ server/sql/k3_tableInsert.sql
@@ -17,26 +17,3 @@ CREATE TABLE IF NOT EXISTS games(
 ) AUTO_INCREMENT=1;
 
 
-CREATE TABLE IF NOT EXISTS pieces (
-	pieceId INT(8) NOT NULL AUTO_INCREMENT,
-    pieceGameId INT(3) NOT NULL,
-    pieceTeamId INT(1) NOT NULL,
-    pieceUnitId INT(2) NOT NULL,
-	piecePositionId INT(4) NOT NULL,
-    pieceContainerId INT(8) NOT NULL,
-    pieceVisible INT(1) NOT NULL,
-    pieceMoves INT(2) NOT NULL,
-    pieceFuel INT(2) NOT NULL,
-    PRIMARY KEY(pieceId)
-) AUTO_INCREMENT=1;
-
-
-CREATE TABLE IF NOT EXISTS item (
-    itemId INT(8) NOT NULL AUTO_INCREMENT,
-    itemGameId INT(3) NOT NULL,
-    itemTeamId INT(1) NOT NULL,
-    itemTypeId INT(2) NOT NULL,
-    PRIMARY KEY(purchaseId)
-) AUTO_INCREMENT=1;
-
-
