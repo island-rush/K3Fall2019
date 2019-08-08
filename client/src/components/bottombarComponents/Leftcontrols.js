c07054501536fa0e9e5c009dbff056e08128c16b
spenceradolph <spenceradolph@ymail.com>
2019-08-08 15:43:23 -0500

    bottom bar controls looking better, no functionality yet

--- client/src/components/bottombarComponents/Leftcontrols.js
+++ client/src/components/bottombarComponents/Leftcontrols.js
@@ -9,12 +9,59 @@ const leftcontrolsStyle = {
 	margin: ".5%"
 };
 
+const buttonStyle = {
+	background: "white",
+	height: "80%",
+	width: "18%",
+	float: "left",
+	margin: "1%",
+	marginTop: "2%",
+	backgroundSize: "100% 100%",
+	backgroundRepeat: "no-repeat"
+};
+
+const buttonImages = {
+	start: { backgroundImage: 'url("./images/buttonImages/iconPlanning.png")' },
+	undo: { backgroundImage: 'url("./images/buttonImages/iconUndo.png")' },
+	cancel: { backgroundImage: 'url("./images/buttonImages/iconCancel.png")' },
+	confirm: { backgroundImage: 'url("./images/buttonImages/iconConfirm.png")' },
+	container: {
+		backgroundImage: 'url("./images/buttonImages/iconContainer.png")'
+	}
+};
+
+const buttonTitles = {
+	start: "Start Planning a Move for a Piece",
+	undo: "Undo a Planned Move",
+	cancel: "Cancel a Plan",
+	confirm: "Confirm a Plan",
+	container: "Open Container Controls Or Something Idk"
+};
+
 class Leftcontrols extends Component {
 	render() {
 		return (
 			<div style={leftcontrolsStyle}>
-				<div>Nothing Here</div>
-				<div>Planning Controls</div>
+				<div
+					title={buttonTitles.start}
+					style={{ ...buttonStyle, ...buttonImages.start }}
+				/>
+				<div
+					title={buttonTitles.cancel}
+					style={{ ...buttonStyle, ...buttonImages.cancel }}
+				/>
+				<div
+					title={buttonTitles.undo}
+					style={{ ...buttonStyle, ...buttonImages.undo }}
+				/>
+				<div
+					title={buttonTitles.container}
+					style={{ ...buttonStyle, ...buttonImages.container }}
+				/>
+				<div
+					title={buttonTitles.confirm}
+					style={{ ...buttonStyle, ...buttonImages.confirm }}
+				/>
 			</div>
 		);
 	}
